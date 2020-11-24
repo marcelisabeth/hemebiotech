05c06a57053459bf2a01309b3969fbb2deeb40b6
melisabeth <melisabeth@LCSDSIMEKS.citadelle.local>
2020-11-24 13:40:26 -0400

    second commit

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,53 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AnalyticsCounter {
+
+	public static void main(String[] args) throws Exception {
+
+//déclaration de mes deux fichiers textes
+		
+//Ce fichier représente la liste des symptomes		
+		String fileListSymptoms = "Project02Eclipse/symptoms.txt";
+		
+// Ce fichier récupére le résultat de l'opération de classement		
+		String fileResult = "Project02Eclipse/FileResult.out";
+
+// Lecture de ma liste de symptomes par le programme
+		FileReader fileReader = new FileReader(fileListSymptoms);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String listSymptoms;
+		
+		
+// Déclaration de ma variable liste		
+		List<String> lineList = new ArrayList<String>();
+		while ((listSymptoms = bufferedReader.readLine()) != null) {
+			lineList.add(listSymptoms);
+			
+// Affichage a l'écran de ma liste de symptomes dans mon fichier original			
+			System.out.println (listSymptoms);
+		}
+		fileReader.close();
+		
+
+// Classement par odre alphabétique
+		Collections.sort(lineList);
+
+		FileWriter fileWriter = new FileWriter(fileResult);
+		PrintWriter out = new PrintWriter(fileWriter);
+		for (String outputLine : lineList) {
+			out.println(outputLine);
+		}
+		out.flush();
+		out.close();
+		fileWriter.close();
+
+	}
+}
