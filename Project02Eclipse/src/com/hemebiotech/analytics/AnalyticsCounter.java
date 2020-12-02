65ccf04ee30d240e7c4c90435727b265488c86ae
melisabeth <melisabeth@LCSDSIMEKS.citadelle.local>
2020-12-02 13:37:03 -0400

    Rajout et simplification de code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,17 +6,20 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class AnalyticsCounter {
 
 	public static void main(String[] args) throws Exception {
 
 //déclaration de mes deux fichiers textes
-		
+
 //Ce fichier représente la liste des symptomes		
 		String fileListSymptoms = "Project02Eclipse/symptoms.txt";
-		
+
 // Ce fichier récupére le résultat de l'opération de classement		
 		String fileResult = "Project02Eclipse/FileResult.out";
 
@@ -24,31 +27,31 @@ public class AnalyticsCounter {
 		FileReader fileReader = new FileReader(fileListSymptoms);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		String listSymptoms;
-		
-		
+
 // Déclaration de ma variable liste		
 		List<String> lineList = new ArrayList<String>();
 		while ((listSymptoms = bufferedReader.readLine()) != null) {
 			lineList.add(listSymptoms);
-			
+
 // Affichage a l'écran de ma liste de symptomes dans mon fichier original			
-			System.out.println (listSymptoms);
+			System.out.println(listSymptoms);
 		}
 		fileReader.close();
-		
-		
-		
-// Classement par odre alphabétique
-		Collections.sort(lineList);
+
+// Classement par ordre alphabétique
+
+		Set<String> lt = new TreeSet<String>(lineList);
 
 		FileWriter fileWriter = new FileWriter(fileResult);
-		PrintWriter out = new PrintWriter(fileWriter);
-		for (String outputLine : lineList) {
-			out.println(outputLine);
+//		PrintWriter out = new PrintWriter(fileWriter);
+		for (String s : lt) {
+//			out.println(s + ": " + Collections.frequency(lineList, s));
+			fileWriter.write(s + ": " + Collections.frequency(lineList, s) + "\n");
+			
 		}
-		out.flush();
-		out.close();
+//		out.flush();
+//		out.close();
 		fileWriter.close();
-
+        
 	}
 }
